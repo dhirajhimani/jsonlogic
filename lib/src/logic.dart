89d013601deb39c3fb8e04e59a52debe5ad6abba
Dhiraj Himani <dhirajhimani@gmail.com>
2023-12-05 11:59:11 +0100

    -- update logic for this use case rule {"==": ["Option A",{"var": "some-key"}]}
    data
    {"some-key": ["Option A"]}

--- lib/src/logic.dart
+++ lib/src/logic.dart
@@ -27,6 +27,9 @@ bool isEqual(Applier applier, dynamic data, List params) {
   }
   var v1 = applier(params[0], data);
   var v2 = applier(params[1], data);
+  if (v1 is List || v2 is List) {
+    return compareContent(v1, v2);
+  }
   if (v1 is String || v2 is String) {
     return toString(v1) == toString(v2);
   }
@@ -34,6 +37,35 @@ bool isEqual(Applier applier, dynamic data, List params) {
     return truth(v1) == truth(v2);
   }
   return v1 == v2;
+}
+
+bool compareContent(dynamic first, dynamic second) {
+  if (first is List && second is List) {
+    return listEquals(first, second);
+  } else if (first is List) {
+    return listEquals(first, [second]);
+  } else if (second is List) {
+    return listEquals([first], second);
+  }
+  return false;
+}
+
+bool listEquals<T>(List<T>? a, List<T>? b) {
+  if (a == null) {
+    return b == null;
+  }
+  if (b == null || a.length != b.length) {
+    return false;
+  }
+  if (identical(a, b)) {
+    return true;
+  }
+  for (int index = 0; index < a.length; index += 1) {
+    if (a[index] != b[index]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 dynamic equalOperator(Applier applier, dynamic data, List params) {
